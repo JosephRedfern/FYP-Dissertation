7f9ae252c0492a7f45ecd69bbdf395d45d5d1831
Joseph Redfern <joseph@redfern.me>
2015-04-30 11:01:48 +0100

    added changes, start detailing algorithms

--- genpdf.sh
+++ genpdf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
 pdflatex --shell-escape main.tex
+bibtex main
+pdflatex --shell-escape main.tex
+pdflatex --shell-escape main.tex
+rm main-blx.bib main.aux main.bbl main.blg main.lol main.out main.run.xml main.toc main.log
 texcount main.tex MainBody/*.tex Support/*.tex
 open main.pdf
--- main.lol
+++
@@ -1,2 +0,0 @@
-\defcounter {refsection}{0}\relax 
-\contentsline {lstlisting}{\numberline {3.1}Acquiring Depth and RGB in Matlab}{17}{lstlisting.3.1}
